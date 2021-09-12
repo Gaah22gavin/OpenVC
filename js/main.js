20e8020892614cb77f6e85aab15c495e4315ca35
Gaah22gavin <89858687+Gaah22gavin@users.noreply.github.com>
2021-09-12 12:48:14 +0200

    forth

--- js/main.js
+++ js/main.js
@@ -128,3 +128,14 @@ var ecco = document.getElementsByClassName("none");
 $('#more').on('click', e=>{
   $('.none').toggleClass('acti');
 })
+
+
+
+$(document).ready(function(){
+	$('.acc h3').click(function(){
+		$(this).next('.content').slideToggle();
+		$(this).parent().toggleClass('active');
+		$(this).parent().siblings().children('.content').slideUp();
+		$(this).parent().siblings().removeClass('active');
+	});
+});
